14db80ad1676b54096a8b653a777b33752e45a6b
Daniel Olshansky <olshansky.daniel@gmail.com>
2020-12-19 15:37:16 -0500

    Add email validation

--- js/submit.js
+++ js/submit.js
@@ -1,6 +1,17 @@
+function validateEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
 // Source: https://stefano.brilli.me/google-forms-html-exporter/
 $('#bootstrapForm').submit(function (event) {
     event.preventDefault();
+    var emailField = $("#1208775221");
+    console.log(emailField.val(), validateEmail(emailField.val()));
+    if (!validateEmail(emailField.val())) {
+      emailField[0].setCustomValidity('Please enter a valid email');
+      return;
+    }
     $('#form-button').prop('disabled', true);
     $('.spinner-border').attr("hidden", false);
     var extraData = {}
